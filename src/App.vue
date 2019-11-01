744c148f3db87dcb2f7d71d8dee4c73aebbf61f9
johnwanjema <jonwanjema@gmail.com>
2019-11-01 11:39:12 +0300

    added routes to coins

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <img src="./assets/logo.png">
      <router-link :to="{ name: 'Hello' }">Home</router-link>
     <router-link to="/about">About</router-link>
+    <router-link to="/coins/ethereum">Ethereum</router-link>
+    <router-link to="/coins/bitcoin">Bitcoin</router-link>
     <router-view/>
   </div>
 </template>
